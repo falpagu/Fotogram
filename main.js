a808f1219811f5fdcda7f059c837e8005c403264
Fesih Alpagu <alpagu.fesih@gmx.at>
2026-01-22 19:05:30 +0100

    Erstes Commit: HTML-Projekt

---
+++ main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const myPics = [
+    "pic_1.jpg",
+    "pic_2.jpg",
+    "pic_3.jpg",
+    "pic_4.jpg",
+    "pic_5.jpg",
+    "pic_6.jpg",
+    "pic_7.jpg",
+    "pic_8.jpg",
+    "pic_9.jpg"
+  
+];
+
+const gallery = document.getElementById("gallery");
+const dialog = document.getElementById("lightbox");
+const closeBtn = document.getElementById("close");
+const dialogImg = document.getAnimations("dialog-img");
